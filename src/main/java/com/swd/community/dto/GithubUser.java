1c88fe3fa9cdd66ec10d6f26710e087e4bdb8579
Swd12345678 <1450741128@qq.com>
2020-04-14 16:47:23 +0800

    @Data标签，添加了user的头像的url

--- src/main/java/com/swd/community/dto/GithubUser.java
+++ src/main/java/com/swd/community/dto/GithubUser.java
@@ -1,34 +1,15 @@
 package com.swd.community.dto;
 
+import lombok.Data;
+
 /**
  * Created by myth on 2020/4/10 18:20
  */
+@Data
 public class GithubUser {
     private String name;
     private Long id;
     private String bio;
+    private String avatar_url;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
 }
